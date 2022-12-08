7b52a754a8519258009e486aa0626a210fd94299
Aiyappa Devaiah <adevaiah@hometap.com>
2022-12-08 01:46:25 -0500

    File data read and info implemented

--- client.c
+++ client.c
@@ -6,21 +6,71 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
+
+int getUserInputInteger() {
+    int result;
+    char *line = NULL;
+    size_t len = 0;
+    while (1) {
+       if (getline(&line, &len, stdin) == -1) {
+           /* eof or error, do whatever is sensible in your case */
+           exit(EXIT_FAILURE);
+       }
+       if (sscanf(line, "%d", &result) != 1) {
+           printf("Invalid choice, please try again: ");
+           continue;
+       }
+       break;
+    }
+    return result;
+}
+
+char * buildFileReadMessage(int size) {
+    char path[50];
+    printf("Enter full path\n");
+    scanf("%49[^\n]%*c", path);
+    char* message_to_server;
+    char* m_size = malloc(sizeof(char)*(int)log10(size));
+    sprintf(m_size, "%d", size);
+    message_to_server = malloc(3+1+strlen(path)+1+strlen(m_size)+1);
+    strcpy(message_to_server, "GET,");
+    strcat(message_to_server, path);
+    strcat(message_to_server, ",");
+    strcat(message_to_server, m_size);
+    return message_to_server;
+}
+
+char * buildFileInfoMessage() {
+    char path[50];
+    printf("Enter full path\n");
+    scanf("%49[^\n]%*c", path);
+    char* message_to_server;
+    message_to_server = malloc(3+1+strlen(path)+1);
+    strcpy(message_to_server, "INF,");
+    strcat(message_to_server, path);
+    return message_to_server;
+}
+
+int send_message_to_server(char * message, int socket_desc) {
+    // Send the message to server:
+    if(send(socket_desc, message, strlen(message), 0) < 0){
+        printf("Unable to send message\n");
+        return -1;
+    }
+    return 0;
+}
 
 int main(void)
 {
   int socket_desc;
   struct sockaddr_in server_addr;
-  char server_message[2000], client_message[2000];
-  
-  // Clean buffers:
-  memset(server_message,'\0',sizeof(server_message));
-  memset(client_message,'\0',sizeof(client_message));
-  
   // Create socket:
   socket_desc = socket(AF_INET, SOCK_STREAM, 0);
   
@@ -43,26 +93,46 @@ int main(void)
   }
   printf("Connected with server successfully\n");
   
-  // Get input from the user:
-  printf("Enter message: ");
-  gets(client_message);
-  
-  // Send the message to server:
-  if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-    printf("Unable to send message\n");
-    return -1;
-  }
-  
-  // Receive the server's response:
-  if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
-    printf("Error while receiving server's msg\n");
-    return -1;
-  }
-  
-  printf("Server's response: %s\n",server_message);
-  
-  // Close the socket:
-  close(socket_desc);
-  
-  return 0;
+  int keepRunning = 1;
+    char dataScanned;
+    while (keepRunning == 1) {
+        printf("Enter choice\n 1:Read file \n 2:Get file info\n 3:Create folder\n 4:Create file\n 5:Delete file\n");
+        int choice = getUserInputInteger();
+        if (choice == 1) {
+            printf("Enter size of file to read\n");
+            int size = getUserInputInteger();
+            char * message = buildFileReadMessage(size);
+            send_message_to_server(message, socket_desc);
+            char response[size];
+            // Receive the server's response:
+            if(recv(socket_desc, response, sizeof(response), 0) < 0){
+                printf("Error while receiving server's msg\n");
+                return -1;
+            }
+            else {
+                printf("Received data: %s\n", response);
+            }
+        }
+        else if (choice == 2) {
+            char * message = buildFileInfoMessage();
+            send_message_to_server(message, socket_desc);
+            char response[500];
+            // Receive the server's response:
+            if(recv(socket_desc, response, sizeof(response), 0) < 0){
+                printf("Error while receiving server's msg\n");
+                return -1;
+            }
+            else {
+                printf("Received data: %s\n", response);
+            }
+        }
+        else {
+            printf("Invalid choice\n");
+        }
+        printf("Enter choice if you wish to continue\n 1: Keep Running\n Any other integer to quit\n");
+        keepRunning = getUserInputInteger();
+    }
+    // Close the socket:
+    close(socket_desc);
+    return 0;
 }
--- server.c
+++ server.c
@@ -10,16 +10,93 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+
+char * get_request = "GET";
+char * inf_request = "INF";
+char root_volume1[] = "/Volumes/FS1/";
+
+int send_response(int client_sock, char* server_message) {
+    if (send(client_sock, server_message, strlen(server_message), 0) < 0){
+    printf("Can't send\n");
+    return -1;
+  }
+  return 0;
+}
+
+char * build_file_info_msg(struct stat stats, char* filedata) {
+    strcpy(filedata, "File permissions: ");
+    if (stats.st_mode & R_OK)
+        strcat(filedata,"r ");
+    if (stats.st_mode & W_OK)
+        strcat(filedata,"w ");
+    if (stats.st_mode & X_OK)
+        strcat(filedata,"x ");
+    char *size;
+    sprintf(size, "File size: %lld ", stats.st_size);
+    strcat(filedata, size);
+
+    struct tm dt;
+    dt = *(gmtime(&stats.st_ctime));
+    char * created_date = malloc(sizeof(char) * 50);
+    sprintf(created_date, "Created on: %d-%d-%d %d:%d:%d ", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,
+                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
+    strcat(filedata, created_date);
+
+    dt = *(gmtime(&stats.st_mtime));
+    char *modified_date = malloc(sizeof(char) * 50);
+    sprintf(modified_date, "Modified on: %d-%d-%d %d:%d:%d ", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,
+                                              dt.tm_hour, dt.tm_min, dt.tm_sec);
+    strcat(filedata, modified_date);
+    return filedata;
+
+}
+
+char * get_file_info(char * filename) {
+    struct stat stats;
+    char * full_path1 = malloc(strlen(root_volume1) + strlen(filename) + 1);
+    strcpy(full_path1, root_volume1);
+    strcat(full_path1, filename);
+    if (stat(full_path1, &stats) == 0)
+    {
+        char data[500];
+        char * response = build_file_info_msg(stats, data);
+        printf("File info retrieved: %s\n", response);
+        return response;
+    }
+    else
+    {
+        printf("Unable to get file properties.\n");
+        return NULL;
+    }
+}
+
+char * get_file_data(char * filename, int size) {
+    FILE* ptr;
+    char * server_message = malloc(sizeof(char) * size);
+    char * full_path1 = malloc(strlen(root_volume1) + strlen(filename) + 1);
+    strcpy(full_path1, root_volume1);
+    strcat(full_path1, filename);
+    ptr = fopen(full_path1, "r");
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+    else {
+        fgets(server_message, size, ptr);
+    }
+    fclose(ptr);
+    return server_message;
+}
 
 int main(void)
 {
   int socket_desc, client_sock;
   socklen_t client_size;
   struct sockaddr_in server_addr, client_addr;
-  char server_message[8196], client_message[8196];
-  
-  // Clean buffers:
-  memset(server_message, '\0', sizeof(server_message));
+  char client_message[4098];
+
   memset(client_message, '\0', sizeof(client_message));
   
   // Create socket:
@@ -68,19 +145,28 @@ int main(void)
     printf("Couldn't receive\n");
     return -1;
   }
-  printf("Msg from client: %s\n", client_message);
-  
-  // Respond to client:
-  strcpy(server_message, "This is the server's response message.");
-  
-  if (send(client_sock, server_message, strlen(server_message), 0) < 0){
-    printf("Can't send\n");
-    return -1;
+  printf("Msg received from client\n");
+  char * data = strdup(client_message);
+  char * request_type = strtok(data, ",");
+  if (strcmp(request_type, get_request) == 0) {
+    char * filename = strtok(NULL, ",");
+    int size = atoi(strtok(NULL, ",")) + 1;
+    printf("Attempting to read file data\n");
+    printf("Filename: %s\n", filename);
+    char * server_message = get_file_data(filename, size);
+    send_response(client_sock, server_message);
   }
-  
+  else if (strcmp(request_type, inf_request) == 0) {
+    printf("Attempting to read file info\n");
+    char * filename = strtok(NULL, ",");
+    printf("Filename: %s\n", filename);
+    char * server_message = get_file_info(filename);
+    send_response(client_sock, server_message);
+  }
+
   // Closing the socket:
   close(client_sock);
   close(socket_desc);
-  
+
   return 0;
 }
